b01ee9b3516545e5fe1973176a4710eaa56737de
Chetan Kumawat <107295989+ChetanK203@users.noreply.github.com>
2023-07-24 21:04:52 +0530

    Add files via upload

---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://is1-ssl.mzstatic.com/image/thumb/Purple124/v4/61/cd/a5/61cda524-818e-7198-a368-355c80430098/AppIcon-1x_U007emarketing-0-7-0-85-220.png/1200x600wa.png';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
